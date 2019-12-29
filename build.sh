5ffb8c42612e787b4e515ab66b79b3fafec9fd02
Daniel Mulholland <dan.mulholland@gmail.com>
2019-12-29 14:43:32 +1300

    Plodding along

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while sleep 1 ;
-  do find . -name '*.adoc' | entr -d asciidoctor-pdf --template-require ./base/template.js -S unsafe asciidoctor-cheat-sheet.adoc; 
+  do find . -name '*.adoc' -or -name '*.js' -or -name '*.css'| entr -d asciidoctor-pdf --template-require ./base/template.js -S unsafe asciidoctor-cheat-sheet.adoc; 
 done
 
